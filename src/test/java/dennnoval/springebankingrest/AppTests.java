286f484a60c023e81765e955ecf2c6cfeacb16d3
dennnoval <dennnoval@gmail.com>
2022-06-24 21:30:40 +0700

    Create AppTests.java

---
+++ src/test/java/dennnoval/springebankingrest/AppTests.java
@@ -0,0 +1,14 @@
+package dennnoval.springebankingrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AppTests {
+
+  @Test
+  void exampeTest() {
+    // Write here
+  }
+
+}
